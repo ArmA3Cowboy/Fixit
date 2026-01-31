c7530ead00ea12487a3d0cbeeb11d9f7e26a64b8
Cowboy <132313680+ArmA3Cowboy@users.noreply.github.com>
2026-01-30 18:50:24 -0700

    Add files via upload

---
+++ config.lua
@@ -0,0 +1,17 @@
+Config = {}
+
+-- Distance away the tow truck spawns (RP)
+Config.MinSpawnDistance = 200
+Config.MaxSpawnDistance = 400
+
+-- Tow vehicle model
+Config.TowTruckModel = 'utillitruck3'
+
+-- AI driver model
+Config.DriverPed = 's_m_y_dockwork_01'
+
+-- Blips
+Config.TowBlipSprite = 544  -- Wrench
+
+-- AI driving speed
+Config.DriveSpeed = 20.0
