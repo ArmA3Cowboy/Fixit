9c446ff66ebfa9afc77e4964054dd0b78f2c16f7
Cowboy <132313680+ArmA3Cowboy@users.noreply.github.com>
2026-01-30 18:46:06 -0700

    Delete server directory

--- server/main.lua
+++
@@ -1,7 +0,0 @@
-RegisterNetEvent("nd_tow:requestTow", function(coords)
-    TriggerClientEvent("nd_tow:newTow", -1, coords)
-end)
-
-RegisterNetEvent("nd_tow:requestRepair", function(coords)
-    TriggerClientEvent("nd_tow:newRepair", -1, coords)
-end)
